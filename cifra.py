ae3b8aea193312c86cbf91107df8f61c8eb4d455
João <etiontdn@gmail.com>
2026-09-24 14:27:14 -0300

    Alterado cifra.py para exportar funções

--- cifra.py
+++ cifra.py
@@ -1,10 +1,3 @@
-'''
-Exercício com Cifra de Cesar.
-Implementar um algoritmo da cifra de César, em qualquer linguagem.
-O deslocamento (chave k) pode variar de 1 a N.
-Pode contemplar somente a encriptação (a decriptação não é obrigatória)
-'''
-
 def encrypt(text, k):
     result = ""
     for char in text:
@@ -31,24 +24,25 @@ def decrypt(text, k):
             result += char
     return result
 
-# Input definindo a chave k
-k = int(input("Digite a chave k (1 a N): "))
-if k < 1:
-    print("A chave k deve ser maior ou igual a 1.")
-    exit()
 
-# Input definindo se quer encriptar ou decriptar
-operation = input("Digite 'e' para encriptar ou 'd' para decriptar: ")
+# Este bloco só roda quando você executa diretamente 'python cifra.py'
+if __name__ == "__main__":
+    k = int(input("Digite a chave k (1 a N): "))
+    if k < 1:
+        print("A chave k deve ser maior ou igual a 1.")
+        exit()
 
-while operation.lower() not in ['e', 'd']:
-    print("Operação inválida. Digite 'e' para encriptar ou 'd' para decriptar.")
     operation = input("Digite 'e' para encriptar ou 'd' para decriptar: ")
 
-if operation.lower() == 'e':
-    text = input("Digite o texto a ser encriptado: ")
-    encrypted_text = encrypt(text, k)
-    print("Texto encriptado:", encrypted_text)
-elif operation.lower() == 'd':
-    text = input("Digite o texto a ser decriptado: ")
-    decrypted_text = decrypt(text, k)
-    print("Texto decriptado:", decrypted_text)
+    while operation.lower() not in ['e', 'd']:
+        print("Operação inválida. Digite 'e' para encriptar ou 'd' para decriptar.")
+        operation = input("Digite 'e' para encriptar ou 'd' para decriptar: ")
+
+    if operation.lower() == 'e':
+        text = input("Digite o texto a ser encriptado: ")
+        encrypted_text = encrypt(text, k)
+        print("Texto encriptado:", encrypted_text)
+    elif operation.lower() == 'd':
+        text = input("Digite o texto a ser decriptado: ")
+        decrypted_text = decrypt(text, k)
+        print("Texto decriptado:", decrypted_text)
